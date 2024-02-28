87bba6b4c8542bca010fb3b4480f16803c1b3f62
Janno <jan-oliver.reincke@fau.de>
2024-02-28 21:19:01 +0100

    neues Skript, das XML-Files in Dataframes umwandelt, bei der jeder einzelne Ticker-Beitrag eine eigene Zeile mit zugeordneter Spielminute erhält

--- datenbereinigung.R
+++ datenbereinigung.R
@@ -45,11 +45,80 @@ save(df_BL_1718, file = "./weltfussball_liveticker/df_BL_1718.RData")
 df_BL_1819 <- dataframes[[2]]
 save(df_BL_1819, file = "./weltfussball_liveticker/df_BL_1819.RData")
 df_BL_1920 <- dataframes[[3]]
-save(df_BL_1819, file = "./weltfussball_liveticker/df_BL_1920.RData")
+save(df_BL_1920, file = "./weltfussball_liveticker/df_BL_1920.RData")
 df_BL_2021 <- dataframes[[4]]
-save(df_BL_1819, file = "./weltfussball_liveticker/df_BL_2021.RData")
+save(df_BL_2021, file = "./weltfussball_liveticker/df_BL_2021.RData")
 df_BL_2122 <- dataframes[[5]]
-save(df_BL_1819, file = "./weltfussball_liveticker/df_BL_2122.RData")
+save(df_BL_2122, file = "./weltfussball_liveticker/df_BL_2122.RData")
 df_BL_2223 <- dataframes[[6]]
-save(df_BL_1819, file = "./weltfussball_liveticker/df_BL_2223.RData")
+save(df_BL_2223, file = "./weltfussball_liveticker/df_BL_2223.RData")
 
+#Zusammenfügen der 6 Saisons zu einem Dataframe
+
+df_BL_1718_bis_2223 <- bind_rows(df_BL_1718, df_BL_1819, df_BL_1920,
+                                 df_BL_2021, df_BL_2122, df_BL_2223)
+save(df_BL_1718_bis_2223, file = "./weltfussball_liveticker/df_BL_1718_bis_2223")
+
+
+#-------------------------------------------------------------------------------
+
+#dataframes importieren (Output-files von import_weltfussball_as_single_entries.R)
+
+pt_df_files <- c(
+  "./weltfussball_liveticker/df_weltfussball_1718_pt.RData",
+  "./weltfussball_liveticker/df_weltfussball_1819_pt.RData",
+  "./weltfussball_liveticker/df_weltfussball_1920_pt.RData",
+  "./weltfussball_liveticker/df_weltfussball_2021_pt.RData",
+  "./weltfussball_liveticker/df_weltfussball_2122_pt.RData",
+  "./weltfussball_liveticker/df_weltfussball_2223_pt.RData"
+)
+
+pt_dataframes <- list()
+for(datei in pt_df_files) {
+  load(datei)
+  pt_dataframes[[datei]] <- df_gesamt
+}
+
+#Dataframes bereinigen
+
+for(i in 1:length(pt_dataframes)) {
+  
+  #Title enthält Ergebnis, Saison, Wettbewerb und Spieltag, die in eigene Spalten überführt werden sollen
+  pt_dataframes[[i]] <- pt_dataframes[[i]] %>% 
+    mutate(Result = str_extract(Title, "\\d+:\\d+")) %>% 
+    separate(Result, c("Result_Home", "Result_Away"), sep=":", remove=FALSE, convert=TRUE) %>% 
+    mutate(Season = str_extract(Title, "\\d{4}/\\d{4}")) %>% 
+    mutate(Competition = str_extract(Title, "(?<=\\().+?(?=\\d{4}/\\d{4})")) %>% 
+    mutate(Matchday = str_extract(Title, "\\d+\\. Spieltag")) %>% 
+    mutate(Date = str_extract(Date, "\\d+-\\d+-\\d+")) %>%
+    
+    #um Teams später als Factors verwenden zu können
+    mutate_at(vars(Team1,Team2), factor)
+  
+  #Spalten-Reihenfolge neu ordnen (ohne Title)
+  neue_reihenfolge <- c("Team1", "Team2", "Matchday", "Competition", "Season", "Date", "Kickoff", "Result", "Time", "Text")
+  pt_dataframes[[i]] <- pt_dataframes[[i]][, neue_reihenfolge]
+}
+
+#bereinigte Dataframes speichern
+
+df_BL_1718_pt <- pt_dataframes[[1]]
+save(df_BL_1718_pt, file = "./weltfussball_liveticker/df_BL_1718_pt.RData")
+df_BL_1819_pt <- pt_dataframes[[2]]
+save(df_BL_1819_pt, file = "./weltfussball_liveticker/df_BL_1819_pt.RData")
+df_BL_1920_pt <- pt_dataframes[[3]]
+save(df_BL_1920_pt, file = "./weltfussball_liveticker/df_BL_1920_pt.RData")
+df_BL_2021_pt <- pt_dataframes[[4]]
+save(df_BL_2021_pt, file = "./weltfussball_liveticker/df_BL_2021_pt.RData")
+df_BL_2122_pt <- pt_dataframes[[5]]
+save(df_BL_2122_pt, file = "./weltfussball_liveticker/df_BL_2122_pt.RData")
+df_BL_2223_pt <- pt_dataframes[[6]]
+save(df_BL_2223_pt, file = "./weltfussball_liveticker/df_BL_2223_pt.RData")
+
+#Zusammenfügen der 6 Saisons zu einem Dataframe
+
+df_BL_1718_bis_2223_pt <- bind_rows(df_BL_1718_pt, df_BL_1819_pt, df_BL_1920_pt,
+                                 df_BL_2021_pt, df_BL_2122_pt, df_BL_2223_pt)
+save(df_BL_1718_bis_2223_pt, file = "./weltfussball_liveticker/df_BL_1718_bis_2223_pt")
+
+print("done")
---
+++ import_weltfussball_as_single_entries.R
@@ -0,0 +1,88 @@
+library(dplyr)
+library(XML)
+library(xml2)
+library(readxl)
+
+#Skript zum Import von Saison-Korpora als xml-Files von weltfussball.de
+#Output: Dateien mit Dataframes, pro Saison ein df mit je einer Zeile pro p-Tag mit zugehörigem time-Tag
+
+#---Variablen---
+
+#Dateipfade der Rohdaten
+files <- list(
+  "./weltfussball_liveticker/bundesliga-2017-2018.xml", 
+  "./weltfussball_liveticker/bundesliga-2018-2019.xml", 
+  "./weltfussball_liveticker/bundesliga-2019-2020.xml", 
+  "./weltfussball_liveticker/bundesliga-2020-2021.xml", 
+  "./weltfussball_liveticker/bundesliga-2021-2022.xml", 
+  "./weltfussball_liveticker/bundesliga-2022-2023.xml"
+)
+
+#Dateipfade zum Export der Dataframes
+
+fnames <- list(
+  "./weltfussball_liveticker/df_weltfussball_1718_pt.RData",
+  "./weltfussball_liveticker/df_weltfussball_1819_pt.RData",
+  "./weltfussball_liveticker/df_weltfussball_1920_pt.RData",
+  "./weltfussball_liveticker/df_weltfussball_2021_pt.RData",
+  "./weltfussball_liveticker/df_weltfussball_2122_pt.RData",
+  "./weltfussball_liveticker/df_weltfussball_2223_pt.RData"
+)
+
+#---Funktionen---
+
+#Funktion zum extrahieren der Metadaten aus der XML-Struktur der Metadaten pro Spiel
+extrahiere_metadaten <- function(text_element) {
+  list(
+    Title = xml_text(xml_find_first(text_element, ".//title")),
+    Team1 = xml_text(xml_find_first(text_element, ".//team1")),
+    Team2 = xml_text(xml_find_first(text_element, ".//team2")),
+    Date = xml_text(xml_find_first(text_element, ".//date")),
+    Kickoff = xml_text(xml_find_first(text_element, ".//kickoff"))
+  )
+}
+
+#---Skript---
+
+for (i in 1:length(files)) {
+  
+  print("File start")
+  
+  xml_file <- read_xml(files[[i]])
+  
+  text_tags <- xml_find_all(xml_file, "//text")
+  
+  df_gesamt <- data.frame()
+  
+  iteration_count <- 0
+  
+  for (text_tag in text_tags) {
+    
+    iteration_count <- iteration_count + 1
+    
+    metadata <- extrahiere_metadaten(text_tag)
+    
+    p_tags <- xml_find_all(text_tag, ".//p")
+    p_text <- xml_text(p_tags)
+    
+    time_tags <- xml_find_all(text_tag, ".//time")
+    time_text <- xml_text(time_tags)
+    
+    df <- data.frame(Title = metadata$Title,
+                     Date = metadata$Date,
+                     Kickoff = metadata$Kickoff,
+                     Team1 = metadata$Team1,
+                     Team2 = metadata$Team2,
+                     Time = time_text,
+                     Text = p_text)
+    
+    df_gesamt <- bind_rows(df_gesamt, df)
+    cat(iteration_count, " done\n")
+    
+  }
+  
+  save(df_gesamt, file = fnames[[i]])
+  
+  print("File done")
+  
+}
